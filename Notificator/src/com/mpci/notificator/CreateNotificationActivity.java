166662868137d2841e51cdfa4af38a60d5115754
artak <artak@artak.(none)>
2013-12-26 17:48:09 +0400

    added alert dialog, second acitivity defined as dialog

--- Notificator/src/com/mpci/notificator/CreateNotificationActivity.java
+++ Notificator/src/com/mpci/notificator/CreateNotificationActivity.java
@@ -2,10 +2,12 @@ package com.mpci.notificator;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -14,6 +16,7 @@ import android.support.v4.app.NotificationCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
 /**
  * This activity is used to create simple notification....
  * 
@@ -23,17 +26,17 @@ public class CreateNotificationActivity extends Activity {
 	@SuppressWarnings("unused")
 	private Button mSend;
 	private EditText mText;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		
-		
+
 	}
+
 	/**
-	 * @brief - This function used to run notification and change activity after clicking on it
-	 *        buttons
+	 * @brief - This function used to run notification and change activity after
+	 *        clicking on it buttons
 	 * 
 	 */
 	@SuppressLint("NewApi")
@@ -41,7 +44,7 @@ public class CreateNotificationActivity extends Activity {
 		Context context = getApplicationContext();
 		mText = (EditText) findViewById(R.id.editText);
 		String message = mText.getText().toString();
-		
+
 		Bitmap largeIcon = BitmapFactory.decodeResource(getResources(),
 				R.drawable.ic_launcher);
 
@@ -49,23 +52,47 @@ public class CreateNotificationActivity extends Activity {
 		// notification is selected
 		NotificationManager nM = (NotificationManager) context
 				.getSystemService(Context.NOTIFICATION_SERVICE);
+		//Intent intent = new Intent(context, NotificationReceiverActivity.class);
 		Intent intent = new Intent(context, NotificationReceiverActivity.class);
 		PendingIntent pintent = PendingIntent
 				.getActivity(context, 0, intent, 0);
 		Notification notification = new NotificationCompat.Builder(context)
 				.setContentTitle("Artush").setContentText(message)
 				.setSmallIcon(R.drawable.ic_stat_notify_images)
-				.setLargeIcon(largeIcon)
-				.build();
+				.setLargeIcon(largeIcon).build();
+
 		notification.contentIntent = pintent;
 		intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
 				| Intent.FLAG_ACTIVITY_SINGLE_TOP);
 
 		// hide the notification after its selected
 		notification.flags |= Notification.FLAG_AUTO_CANCEL;
-		//notification.setLatestEventInfo(context, "Artush", message, pintent);
-		nM.notify(100, notification);
+
+		nM.notify(1, notification);
 
 	}
-	
+	/**
+	 * @brief - This function used to create alert dialog when notification is clicked
+	 * 
+	 */
+	public void Dialog() {
+		mText = (EditText) findViewById(R.id.editText);
+		final String message = mText.getText().toString();
+		AlertDialog.Builder builder = new AlertDialog.Builder(
+				CreateNotificationActivity.this);
+		builder.setTitle("Notification!")
+				.setMessage(message)
+				.setIcon(R.drawable.ic_launcher)
+				.setCancelable(false)
+				.setPositiveButton("Okay",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								dialog.cancel();
+							}
+						});
+		AlertDialog alert = builder.create();
+		alert.show();
+		
+	}
+
 }
--- Notificator/src/com/mpci/notificator/NotificationReceiverActivity.java
+++ Notificator/src/com/mpci/notificator/NotificationReceiverActivity.java
@@ -1,17 +1,45 @@
 package com.mpci.notificator;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
-
+/**
+ * This activity is used to demonstrate example activity after clicking on notification....
+ * 
+ * @author Artak and Artush
+ */
 public class NotificationReceiverActivity extends Activity {
 	//EditText mMessage;
 	//TextView mText;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.result);
-    //mText = (TextView) findViewById(R.id.textView1);
+    Dialog();
+    /*Intent intent = new Intent(this, CreateNotificationActivity.class);
+    startActivity(intent);
+    */
    
+  }
+  
+  void Dialog(){
+	  AlertDialog.Builder builder = new AlertDialog.Builder(
+				this);
+		builder.setTitle("Notification!")
+				.setMessage("message")
+				.setIcon(R.drawable.ic_launcher)
+				.setCancelable(true)
+				.setPositiveButton("Okay",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								dialog.cancel();
+							}
+						});
+		AlertDialog alert = builder.create();
+		alert.show();
+//	    Intent intent = new Intent(this, CreateNotificationActivity.class);
+//		startActivity(intent);
   }
  
   
